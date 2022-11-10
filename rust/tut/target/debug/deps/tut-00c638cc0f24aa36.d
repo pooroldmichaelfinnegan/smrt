5139a6a7bbbb85512727473443afa23009905a91
monte <pooroldmichaelfinnegan@gmail.com>
2022-11-10 22:29:38 +1300

    add react repo for gitignore testing

---
+++ rust/tut/target/debug/deps/tut-00c638cc0f24aa36.d
@@ -0,0 +1,5 @@
+/Users/g/_m/git_221110/smrt/rust/tut/target/debug/deps/tut-00c638cc0f24aa36.rmeta: src/main.rs
+
+/Users/g/_m/git_221110/smrt/rust/tut/target/debug/deps/tut-00c638cc0f24aa36.d: src/main.rs
+
+src/main.rs:
---
+++ rust/tut/target/debug/deps/tut-94da59f1da7d1f0e.d
@@ -0,0 +1,5 @@
+/Users/g/_m/git_221110/smrt/rust/tut/target/debug/deps/tut-94da59f1da7d1f0e.rmeta: src/main.rs
+
+/Users/g/_m/git_221110/smrt/rust/tut/target/debug/deps/tut-94da59f1da7d1f0e.d: src/main.rs
+
+src/main.rs:
