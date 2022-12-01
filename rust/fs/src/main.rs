e8ff71c0ad94536a1232ca92c012ecb53c587052
monte <pooroldmichaelfinnegan@gmail.com>
2022-12-02 04:12:24 +1300

    .gitignore

--- rust/fs/src/main.rs
+++ rust/fs/src/main.rs
@@ -1,18 +1,17 @@
 #![allow(unused)]
 
 use std::fs;
-use std::str;
 
 fn main() -> Result<(), std::io::Error> {
 
-  // let vec = fs::read("files/c0ffee.hex")?; 
-  let vec = fs::read("files/eggs.txt")?; 
+  let vec = fs::read("files/C0FFEE.hex")?;
   println!("{:?}", vec);
 
   let vec2 = vec.iter()
-    .map(|byte| {byte / 2});
+    .map(|byte| { byte / 2 });
   println!("{:?}", vec2.collect::<Vec<u8>>());
 
+<<<<<<< HEAD:rust/fs/src/main.rs
   // println!("{:?}", String::from_utf8(vec).unwrap());
   let mut v: Vec<u8> = vec![];
 
@@ -21,6 +20,9 @@ fn main() -> Result<(), std::io::Error> {
   }
 
   println!("{:?}", v);
+=======
+  println!("{:?}", vec.as_slice());
+>>>>>>> parent of 36c28f8 (eggs.txt):rust/tut/src/main.rs
 
   Ok(())
 }
