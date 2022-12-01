36c28f8fe8ed220b0e29194fac4d770eca0c1302
monte <pooroldmichaelfinnegan@gmail.com>
2022-12-02 01:24:45 +1300

    eggs.txt

--- rust/tut/src/main.rs
+++ rust/tut/src/main.rs
@@ -1,17 +1,19 @@
 #![allow(unused)]
 
 use std::fs;
+use std::str;
 
 fn main() -> Result<(), std::io::Error> {
 
-  let vec = fs::read("files/C0FFEE.hex")?;
+  // let vec = fs::read("files/c0ffee.hex")?; 
+  let vec = fs::read("files/eggs.txt")?; 
   println!("{:?}", vec);
 
   let vec2 = vec.iter()
-    .map(|byte| { byte / 2 });
+    .map(|byte| {byte / 2});
   println!("{:?}", vec2.collect::<Vec<u8>>());
 
-  println!("{:?}", vec.as_slice());
+  println!("{:?}", String::from_utf8(vec).unwrap());
 
   Ok(())
 }
