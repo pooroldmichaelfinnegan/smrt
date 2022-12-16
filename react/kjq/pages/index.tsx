99242a3ea4245f14b8ffc1b68b2c9892d4d4c554
monte <pooroldmichaelfinnegan@gmail.com>
2022-12-17 04:01:39 +1300

    kjq

--- react/kjq/pages/index.tsx
+++ react/kjq/pages/index.tsx
@@ -4,36 +4,57 @@ import styles from '../styles/Home.module.css'
 
 import React, { useState, useRef } from 'react'
 
+const subject = [["i", "jeg"],["you", "du"],["he", "han"],["she", "hun"],["we", "vi"],["you", "i"],["they", "de"]]
+const object = [["me", "mig"],["you", "dig"],["him", "ham"],["her", "hende"],["us", "os"],["you", "jer"],["them", "dem"]]
+
 export default function Home() {
   // const inputRef = useRef()
 
   return (
-    <div className={styles.container}>
-      Home<br />
-      <Card />
+    <div className="border-2 flex flex-row h-full">
+      <div id="sidebar" className="border-2 w-1/3 h-full">
+        <Answers array={subject} />
+      </div>
+      <div className="">
+        home<br />
+        <Card />
+      </div>
     </div>
   )
 }
 
 function Card() {
-  const [inputField, setInputField] = useState('> default <')
+  const [inputField, setInputField] = useState('')
 
-  console.log({ setInputField })
+  return <div className="border-2">
+    card<br />
 
-  return <div className={styles.card}>
-    Card<br />
-    <Input field={inputField} setField={setInputField} /><br />
+    <Input setField={setInputField} /><br />
     {inputField}
   </div>
 }
 
-function Input({ field, setField }) {
+function Input({ setField }) {
   const inputRef = useRef<HTMLInputElement>(null)
 
   return <input
-    className="text-red-900 text-center "
+    className="text-white-900 text-center "
     type="text"
-    ref={inputRef}
-    onChange={() => setField(inputRef.current.value)}
+    onChange={(e) => setField(e.target.value)}
   />
+}
+
+const answers = [["true", "egg"]]
+
+function Answers({ array }) {
+  const a = []
+
+  for (let i = 0; i < array.length; i++) {
+    a.push(<div className="flex flex-row justify-center align-center">
+        <div className="border-2 w-1/3">{array[i][0]}</div>
+        <div className="border-2 w-1/3">{array[i][1]}</div>
+      </div>)
+  }
+
+  return a
 }
