663efc28935709dd0a5037397a3eb954c3ba66e6
monte <pooroldmichaelfinnegan@gmail.com>
2022-12-28 03:27:27 +1300

    corrections

--- react/kjq/pages/index.tsx
+++ react/kjq/pages/index.tsx
@@ -47,61 +47,8 @@ const egg = [
 // import days_of_the_week__ugens_dage from "../arrays/days_of_the_week__ugens_dage.json"
 // import oneToTwenty from "../arrays/numbers.json"
 import simp from "../arrays/simple.json"
-import { prependOnceListener } from "process"
 
-import people from "../arrays/FunEasyLearn/people.json"
-import describing_people from "../arrays/FunEasyLearn/describing_people.json"
-import fashion from "../arrays/FunEasyLearn/fashion.json"
-import body from "../arrays/FunEasyLearn/body.json"
-import health from "../arrays/FunEasyLearn/health.json"
-import house from "../arrays/FunEasyLearn/house.json"
-import shopping from "../arrays/FunEasyLearn/shopping.json"
-import food_drink from "../arrays/FunEasyLearn/food_drink.json"
-import education from "../arrays/FunEasyLearn/education.json"
-import work from "../arrays/FunEasyLearn/work.json"
-import culture from "../arrays/FunEasyLearn/culture.json"
-import leisure_time from "../arrays/FunEasyLearn/leisure_time.json"
-import sport from "../arrays/FunEasyLearn/sport.json"
-import city from "../arrays/FunEasyLearn/city.json"
-import transport from "../arrays/FunEasyLearn/transport.json"
-import communications from "../arrays/FunEasyLearn/communications.json"
-import security from "../arrays/FunEasyLearn/security.json"
-import animals from "../arrays/FunEasyLearn/animals.json"
-import plants from "../arrays/FunEasyLearn/plants.json"
-import environment from "../arrays/FunEasyLearn/environment.json"
-import systems from "../arrays/FunEasyLearn/systems.json"
-import verbs from "../arrays/FunEasyLearn/verbs.json"
-import adjectives from "../arrays/FunEasyLearn/adjectives.json"
-import adverbs from "../arrays/FunEasyLearn/adverbs.json"
-import other from "../arrays/FunEasyLearn/other.json"
-
-const FunEasyLearn = [
-  // ...people,
-  // ...describing_people,
-  // ...fashion,
-  // ...body,
-  // ...health,
-  // ...house,
-  // ...shopping,
-  // ...food_drink,
-  // ...education,
-  ...work,
-  ...culture,
-  ...leisure_time,
-  // ...sport,
-  // ...city,
-  // ...transport,
-  // ...communications,
-  // ...security,
-  // ...animals,
-  // ...plants,
-  // ...environment,
-  // ...systems,
-  // ...verbs,
-  // ...adjectives,
-  // ...adverbs,
-  // ...other
-]
+import { FunEasyLearn } from "../arrays/FunEasyLearn/all"
 
 const arr = FunEasyLearn
 // let withCount = arr.map((words) => {
@@ -294,10 +241,10 @@ function ListOfWords({ list }) {
 
     {array.map((ord) => {
       return <div className="border-[1px] border-gray-800 text-xs flex flex-row justify-center align-center">
-        <div className={`flex-2 py-1 px-3 truncate text-xl`}>{ord[0]}</div>
-        <div className={`flex-1 py-1 px-3 truncate`}>{ord[1]}</div>
-        <div className={`flex-1 py-1 px-3 truncate`}>{ord[2]}</div>
+        <div className={`flex-2 py-1 px-3 truncate text-xg`}>{ord[0]}</div>
+        <div className={`flex-1 py-1 px-3 truncate text-xg`}>{ord[1]}</div>
+        <div className={`flex-1 py-1 px-3 truncate text-xg`}>{ord[2]}</div>
       </div>
     })}
-    </>
+  </>
 }
